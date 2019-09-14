c356a06b5b97e8536c886719690b17af59dbce98
James Marr <meagher.james@gmail.com>
2019-09-14 18:17:38 -0400

    remove the student component css due we are not using it and clean up code

---
+++ src/app/models/parentguard.ts
@@ -0,0 +1,13 @@
+export class ParentGuard {
+    id: number;
+    name: string;
+    addressOne: string;
+    addressTwo: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    phoneNumber: string;
+    altPhoneNumber: string;
+    email: string;
+    altEmail: string;
+}

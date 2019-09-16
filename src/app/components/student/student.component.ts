f67bff799beb54f206ef02f5b503ad0fc15b6368
ifofana <ibrahima.fofana.ia@gmail.com>
2019-09-16 15:09:53 -0400

    Resolved the gender, class day and age group drop-down selection issues.

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -15,29 +15,6 @@ export class StudentComponent implements OnInit {
 
   student: Student;
 
-  // Gender array
-  listGender = [
-    {id: 0, name: 'Male'},
-    {id: 1, name: 'Female'}
-  ];
-
-  // List the class day
-  listDays = [
-    { id: 0, name: 'Saturday'},
-    { id: 1, name: 'Sunday'}
-  ];
-
-  // List the class selection
-  listAgeGroup = [
-    { id: 0, value: 'foundtion', name: 'Foundition - 5 & 6 years old'},
-    { id: 1, value: 'beginner', name: 'Beginner - 7 & 8 years old'},
-    { id: 2, value: 'intermediate', name: 'Intermediate - 9 & 10 years old'},
-    { id: 3, value: 'proficient', name: 'Proficient - 11 & 12 years old'},
-    { id: 4, value: 'advanced', name: 'Advanced - 13 & 14 years old'},
-    { id: 5, value: 'youth', name: 'Youth - 15 & 16 years old'},
-    { id: 6, value: 'adult', name: 'Adult - 17 & 18 years old'}
-  ];
-
   // Flag for the text area, default is hidden
   isShown = false;
 
@@ -62,19 +39,6 @@ export class StudentComponent implements OnInit {
   // When isShown flag is true then the toggleShow be tell the text area to be seen
   toggleShow( ) {
     this.isShown = ! this.isShown;
-  }
-
-  selectDayChange($event: any) {
-    // In my case $event come with a id value
-    this.student.classDay = this.listDays[$event].name;
-  }
-
-  selectAgeGroup($event: any) {
-    this.student.classSelection = this.listAgeGroup[$event].name;
-  }
-
-  selectGender($event: any) {
-    this.student.gender = this.listGender[$event].name;
   }
 
   saveStudent( ) {

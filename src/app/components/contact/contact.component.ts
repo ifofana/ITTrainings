77b27209bf9f6cc68fbe88f23c477609eee8f255
ifofana <ibrahima.fofana.ia@gmail.com>
2019-11-18 14:37:40 -0500

    draft nested forms

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { ContactDataService } from '../../services/contact-data.service';
 import { Contact } from '../../models/contact';
 import { User } from 'src/app/models/user';
@@ -16,8 +16,29 @@ export class ContactComponent implements OnInit {
   contact: Contact;
   currentUser: User;
 
-  constructor(private contactService: ContactDataService, private route: ActivatedRoute, private router: Router) {
+  data = {
+    cities: [
+      {
+        city: "",
+        addressLines: [
+          {
+            addressLine: "",
+          }
+        ]
+      }
+    ]
+  }
+
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private contactService: ContactDataService, private route: ActivatedRoute, private router: Router) {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.myForm = this.fb.group({
+      name: [''],
+      cities: this.fb.array([])
+    })
+
+    this.setCities();
   }// end of parameterized constructor
 
   ngOnInit( ) {
@@ -32,6 +53,55 @@ export class ContactComponent implements OnInit {
         );
     }// end of if statement
   }// end of ngOnInit method
+
+  onSubmit() {
+    alert(this.myForm.value);
+  }
+
+  addNewCity() {
+    let control = <FormArray>this.myForm.controls.cities;
+    control.push(
+      this.fb.group({
+        city: [''],
+        addressLines: this.fb.array([])
+      })
+    )
+  }
+
+  deleteCity(index) {
+    let control = <FormArray>this.myForm.controls.cities;
+    control.removeAt(index)
+  }
+
+  addNewAddressLine(control) {
+    control.push(
+      this.fb.group({
+        addressLine: ['']
+      }))
+  }
+
+  deleteAddressLine(control, index) {
+    control.removeAt(index)
+  }
+
+  setCities() {
+    let control = <FormArray>this.myForm.controls.cities;
+    this.data.cities.forEach(x => {
+      control.push(this.fb.group({ 
+        city: x.city, 
+        addressLines: this.setAddressLines(x) }))
+    })
+  }
+
+  setAddressLines(x) {
+    let arr = new FormArray([])
+    x.addressLines.forEach(y => {
+      arr.push(this.fb.group({ 
+        addressLine: y.addressLine 
+      }))
+    })
+    return arr;
+  }
 
   saveContact() {
     if (this.id === -1) {

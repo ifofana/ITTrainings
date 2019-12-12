9b2e30b46c4b3c6cdfb9ee78921552c46360885f
ifofana <ibrahima.fofana.ia@gmail.com>
2019-12-12 18:17:09 -0500

    added validators to some of student fields

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { ContactDataService } from '../../services/contact-data.service';
-import { Contact } from '../../models/contact';
 import { User } from 'src/app/models/user';
-import { Student } from 'src/app/models/student';
 
 @Component({
   selector: 'app-contact',
@@ -46,16 +44,15 @@ export class ContactComponent implements OnInit {
 
   initStudent() {
     return this._fb.group({
-      firstName: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
       middlename: [''],
-      lastName: ['', Validators.required],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
       gender: [''],
-      dob: [''],
-      age: [''],
+      dob: ['', Validators.required],
+      age: ['', Validators.required],
       allerges: [''],
       classDay: [''],
       classSelection: [''],
-      //isShown: false,
       parentGuardians: this._fb.array([
         this.initParentGuardian()
       ])

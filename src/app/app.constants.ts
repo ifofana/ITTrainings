9a0daf84a7a6f857723b27741ccdf01af6e1f374
ibrah <ifofana75@gmail.com>
2021-12-16 21:27:08 -0500

    new API_URL for a new generated Public IPv4 DNS.

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://ec2-3-82-248-234.compute-1.amazonaws.com:8080';
+export const API_URL = 'http://ec2-3-82-252-142.compute-1.amazonaws.com:8080';
 
 export const AUTHENTICATED_USER = 'currentUser'

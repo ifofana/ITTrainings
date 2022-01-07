ed090faafcb4bc758402cbf9c061084bba041f15
ibrah <ifofana75@gmail.com>
2022-01-07 00:03:56 -0500

    Used aws elastic beanstlk single-instance env: ec2 + Elastic IP.

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://ec2-3-82-252-142.compute-1.amazonaws.com:8080';
+export const API_URL = 'http://springbootawsrdsisgstudentservice-env.eba-wt5crpyw.us-east-1.elasticbeanstalk.com';
 
 export const AUTHENTICATED_USER = 'currentUser'

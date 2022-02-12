c95027ac5837ada52dc6fe766e7a68fa985357b6
ibrah <ifofana75@gmail.com>
2022-02-12 03:03:32 -0500

    Changed API_URL

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,4 @@
-export const API_URL = 'http://springbootawsrdsisgstudentservice-env-1.eba-fkkfryd3.us-east-1.elasticbeanstalk.com';
+//export const API_URL = 'http://localhost:5000';
+export const API_URL = 'http://springbootawsrdsstudentservice-env.eba-dkhwhkey.us-east-1.elasticbeanstalk.com';
 
 export const AUTHENTICATED_USER = 'currentUser'
